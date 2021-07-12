d191eaaac567565660e6a2ed5663b2c784304b13
Adrian Walker <awalker3365@gmail.com>
2021-07-12 14:49:39 -0400

    7/12/21

--- slope.js
+++ slope.js
@@ -1,8 +1,16 @@
 // Rest
 
-let collectAnimals = (...animals) => {
-    animals
-}
+let collectAnimals = (...animals) => animals
 
 collectAnimals("dog", "cat", "mouse", "jackolope", "platypus");
 // ["dog", "cat", "mouse", "jackolope", "platypus"]
+
+// Object Literals
+
+
+
+// let combineFruit = (fruits, sweets, veggies) => {
+//     fruits: ['apple', 'pear'],
+//     sweets: ['cake', 'pie'],
+//     veggies: ['carrot']
+// }t
