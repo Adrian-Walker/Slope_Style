1e8dcc34648be186e7a4b8a77771b5bc8bae715b
Adrian Walker <awalker3365@gmail.com>
2021-07-19 00:46:49 -0400

    7/18/21

--- slope.js
+++ slope.js
@@ -1,8 +1,43 @@
-// Rest
+// Rest & Spread
 
-let collectAnimals = (...animals) => {
-    animals
+const collectAnimals = (...a) => {
+    return [...a]
+}
+let animals = []
+console.log(collectAnimals('pig', 'cat', 'dog', 'fish', 'bear', 'lion',));
+
+//Rest & Spread
+
+const combinedFruit = (fruit, sweets, vegetables) => ({ fruit, sweets, vegetables })
+console.log(combinedFruit(['apple', 'pear'], ['cake', 'ice cream'], ['corn', 'cabbage']))
+
+//Destructuring
+
+const vacation = {
+    location: "Jamaica",
+    duration: "6 months"
+}
+function parseSentence(location, duration) {
+    return "I am headed to ${location} for ${duration}"
+};
+
+//es6
+const items = ['ring', 'hat', 'shoe']
+const returnFirst = (items) => {
+    const firstItem = items;
+    return firstItem[0];
 }
 
-collectAnimals("dog", "cat", "mouse", "jackolope", "platypus");
-// ["dog", "cat", "mouse", "jackolope", "platypus"]
+//Template Literals
+
+const favoriteActivities = ["magnets", "snowboarding", "philanthropy", "janitor work", "eating"];
+
+function returnFavorites(arr) {
+    var firstFav = favoriteActivities[0]
+    var secondFav = favoriteActivities[2]
+    var thirdFav = favoriteActivities[3]
+    return `My top three favorite activities are, ${firstFav} ,  + ${secondFav} , and " + ${thirdFav}`;
+}
+
+returnFavorites(favoriteActivities)
+returnFavorites(favoriteActivities)
