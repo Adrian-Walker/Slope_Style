5f86c887777a2e5e9d81e8a7b6e023aa034fb379
Adrian Walker <awalker3365@gmail.com>
2021-07-19 16:23:22 -0400

    7/19/21 day

--- slope.js
+++ slope.js
@@ -3,7 +3,7 @@
 const collectAnimals = (...a) => {
     return [...a]
 }
-let animals = []
+// let animals = []
 console.log(collectAnimals('pig', 'cat', 'dog', 'fish', 'bear', 'lion',));
 
 //Rest & Spread
@@ -12,21 +12,26 @@ const combinedFruit = (fruit, sweets, vegetables) => ({ fruit, sweets, vegetable
 console.log(combinedFruit(['apple', 'pear'], ['cake', 'ice cream'], ['corn', 'cabbage']))
 
 //Destructuring
-
 const vacation = {
     location: "Jamaica",
-    duration: "6 months"
+    duration: "6 months",
 }
+
 function parseSentence(location, duration) {
     return `I am headed to ${location} for ${duration}`
-};
+}
 
-//es6
+console.log(parseSentence(vacation.location, vacation.duration))
+
+
+//Destructuring
 const items = ['ring', 'hat', 'shoe']
 const returnFirst = (items) => {
     const firstItem = items;
     return firstItem[0];
 }
+
+console.log(returnFirst(items))
 
 //Template Literals
 
@@ -38,3 +43,34 @@ function returnFavorites(arr) {
     var thirdFav = favoriteActivities[3]
     return `My top three favorite activities are, ${firstFav} ,  + ${secondFav} , and " + ${thirdFav}`;
 }
+
+console.log(returnFavorites(favoriteActivities))
+
+// Rest + Spread
+
+const combineAnimals = (...realAnimals, ...magicalAnimals, ...mysteriousAnimals) => {
+    return [realAnimals, magicalAnimals, mysteriousAnimals]
+}
+
+const realAnimals = ["dog", "cat", "mouse"];
+const magicalAnimals = ["jackolope"];
+const mysteriousAnimals = ["platypus"];
+
+console.log(combineAnimals())
+
+
+
+
+
+
+// Make more Es6
+
+const product = (a, b, c, d, e) => {
+    var numbers = [a, b, c, d, e];
+
+    return numbers.reduce((acc, number) => {
+        return acc * number;
+    }, 1)
+}
+
+console.log(product(a,b,c,d,e))
