2170a6f44ed750a4081ccf9122110a00b3e98ed5
Adrian Walker <awalker3365@gmail.com>
2021-07-20 17:29:50 -0400

    slope 2nd

--- slope.js
+++ slope.js
@@ -3,7 +3,6 @@
 const collectAnimals = (...a) => {
     return [...a]
 }
-// let animals = []
 console.log(collectAnimals('pig', 'cat', 'dog', 'fish', 'bear', 'lion',));
 
 //Rest & Spread
@@ -17,51 +16,39 @@ const vacation = {
     duration: "6 months",
 }
 
-function parseSentence(location, duration) {
+function parseSentence({ location, duration }) {
     return `I am headed to ${location} for ${duration}`
 }
 
-console.log(parseSentence(vacation.location, vacation.duration))
+console.log(parseSentence(vacation))
 
 
 //Destructuring
 const items = ['ring', 'hat', 'shoe']
-const returnFirst = (items) => {
-    const firstItem = items;
-    return firstItem[0];
+const returnFirst = (firstItem, ...items) => {
+    return firstItem;
 }
-
-console.log(returnFirst(items))
+console.log(returnFirst(...items))
 
 //Template Literals
 
 const favoriteActivities = ["magnets", "snowboarding", "philanthropy", "janitor work", "eating"];
 
-function returnFavorites(arr) {
-    var firstFav = favoriteActivities[0]
-    var secondFav = favoriteActivities[2]
-    var thirdFav = favoriteActivities[3]
-    return `My top three favorite activities are, ${firstFav} ,  + ${secondFav} , and " + ${thirdFav}`;
+function returnFavorites(firstFav, secondFav, thirdFav, ...arr) {
+    return `My top three favorite activities are, ${firstFav} ,   ${secondFav} , and   ${thirdFav}`;
 }
+console.log(returnFavorites(...favoriteActivities))
 
-console.log(returnFavorites(favoriteActivities))
-
-// Rest + Spread
-
-const combineAnimals = (...realAnimals, ...magicalAnimals, ...mysteriousAnimals) => {
-    return [realAnimals, magicalAnimals, mysteriousAnimals]
-}
-
+// Rest + Spread ****
 const realAnimals = ["dog", "cat", "mouse"];
 const magicalAnimals = ["jackolope"];
 const mysteriousAnimals = ["platypus"];
 
-console.log(combineAnimals())
-
-
-
-
-
+const combineAnimals = (...animals) => {
+    return animals.reduce((a, b) => a.concat(...b), [])
+}
+const allAnimals = combineAnimals(realAnimals, mysteriousAnimals, magicalAnimals)
+console.log(allAnimals)
 
 // Make more Es6y
 
@@ -69,8 +56,20 @@ const product = (a, b, c, d, e) => {
     var numbers = [a, b, c, d, e];
 
     return numbers.reduce((acc, number) => {
-        return acc * number;
+        return 7 * 41;
     }, 1)
 }
+console.log(product())
 
-console.log(product(a,b,c,d,e))
+// Make Function more es6
+
+const arr1 = ['a', 'b', 'c']
+const arr2 = ['d', 'e']
+const arr3 = [...arr1, ...arr2]
+
+const unshift = (arr, ...arr4) => {
+    console.log(arr, arr4)
+    return arr4.concat(arr)
+}
+console.log()
+console.log(unshift(arr2, 'a', 's'))
